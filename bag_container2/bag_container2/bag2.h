0433f23af506d3e77147b51b5efe93d1890cbbec
Samuel Fils <smfils1@gmail.com>
2018-03-20 22:43:13 -0400

    added Documentation

--- bag_container2/bag_container2/bag2.h
+++ bag_container2/bag_container2/bag2.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef BAG2_H
+#define BAG2_H
 
 
 namespace sfils_csc212 {
@@ -6,23 +7,53 @@ namespace sfils_csc212 {
 
 	public:
 		static const size_t CAPACITY = 10;
-		bool insert(int value);
-		bool remove(int value);
-		bool binary_search(int value) const;
-		size_t size() const {
-			return used;
+		bag2()
+		//Precondition: Bag is be sorted. 
+		//Postcondition: Bag is initilized and is set empty.
+		{
+			used = 0;
 		}
+		
 
+		bool insert(int value);
+		//Precondition: Bag is be sorted. 
+		//Postcondition: Value is copied to bag and returns true, otherwise returns false while maintaining an sorted bag. 
+		
+		bool remove(int value);
+		//Precondition: Bag is be sorted 
+		//Postcondition: Value is removed from bag and returns true, otherwise returns false while maintaining an sorted bag. 
+
+		bool binary_search(int value) const;
+		//Precondition: Bag is be sorted.
+		//Postcondition: Value is found and returns true, otherwise returns false. 
+
+		 size_t size() const
+		//Postcondition: Returns size to bag
+		 {		 
+			 return used;
+		 }
+		
 	private:
 		int data[CAPACITY];
-		size_t used = 0;
+		size_t used;
 		size_t insert_position(int value);
+		//Precondition: Bag must be sorted.
+		//Postcondition: Returns value's position of insertion.
+
 		void shift_elements_right(size_t index);
+		//Precondition: Index is the intended position for value insertion. 
+		//Postcondition: Elements are shifted right starting from index.
+
 		int find_position(int value);
+		//Postcondition: Returns index of the first element that equals value. Return -1 if value not found.
+
 		void shift_elements_left(int index);
+		//Precondition: Index is the intended position for value deletion. 
+		//Postcondition: Elements are shifted left starting from index+1.
+
 	};
 }
 
-
+#endif
 
 
--- bag_container2/bag_container2/main.cpp
+++ bag_container2/bag_container2/main.cpp
@@ -5,8 +5,8 @@ using sfils_csc212::bag2;
 
 // This bag data structure has a search time of O(lg n), and a insert/remove time of O(n).
 
-int main() {	
-	
+int main() {
+
 	system("pause");
 	return 0;
 }
